54c2901de3a1137d270290a5b51328014935a0c7
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-30 22:16:21 +0300

    added pics as cards

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import uniqid from "uniqid";
 import plato from "../images/plato.jpeg";
+import aristotle from "../images/aristotle.jpeg";
 
 const Cards = () => {
   const [level1, setLevel1] = useState([
@@ -9,22 +10,30 @@ const Cards = () => {
       img: plato,
       id: uniqid(),
     },
-    { title: "Aristotle", img: "plato", id: uniqid() },
+    { title: "Aristotle", img: aristotle, id: uniqid() },
     { title: "Tyrasymakhos", img: "plato", id: uniqid() },
     { title: "Socrates", img: "plato", id: uniqid() },
   ]);
 
   return (
     <div>
-      yoho
       <div>{level1[0].id}</div>
       <ul>
         {level1.map((item) => {
           return (
             <div>
               <li key={item.id}>
-                {" "}
-                {item.title}, {item.id}, <img src={item.img} />
+                {/* {" "}
+                {item.title}, {item.id}, <img src={item.img} /> */}
+                <div className="card" style={{ width: "18rem" }}>
+                  <img src={plato} className="card-img-top" alt="..."></img>
+                  <div className="card-body">
+                    <p className="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
               </li>
             </div>
           );
