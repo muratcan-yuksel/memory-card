cab5d2f4c0513b119e26dc353b0e8a4ad5781043
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-30 23:19:35 +0300

    managed to get the id of each mapped element

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,6 +6,7 @@ import thrasymachus from "../images/thrasymachus.jpeg";
 import socrates from "../images/socrates.jpg";
 
 const Cards = () => {
+  //initial philosophers array
   const [level1, setLevel1] = useState([
     {
       title: "Plato",
@@ -16,13 +17,21 @@ const Cards = () => {
     { title: "Thrasymachus", img: thrasymachus, id: uniqid() },
     { title: "Socrates", img: socrates, id: uniqid() },
   ]);
+  //selected items array
+  const [selectedItems, setSelectedItems] = useState([]);
+
+  const foo2 = (e) => {
+    console.log(e.target.id);
+  };
   //doing the map here so that I can return elements in a bootstrap grid
-  const foo = level1.map((item) => {
+  const level1Cards = level1.map((item) => {
     return (
-      <div className="col-lg-3 col-sm-6 col-md-6 mt-5 ms-5">
+      <div key={item.id} className="col-lg-3 col-sm-6 col-md-6 mt-5 ms-5">
         <div className="card" style={{ width: "18rem" }}>
           <img
+            onClick={foo2}
             src={item.img}
+            id={item.id}
             className="card-img-top"
             alt="a philosopher"
           ></img>
@@ -35,8 +44,8 @@ const Cards = () => {
   });
   return (
     <div>
-      <div class="container">
-        <div className="row">{foo}</div>
+      <div className="container">
+        <div className="row">{level1Cards}</div>
       </div>
     </div>
   );
