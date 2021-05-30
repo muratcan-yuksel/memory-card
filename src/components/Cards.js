0e5bece76b7546c1a1eb02319c30d5f93ee6a737
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-30 22:41:16 +0300

    UUUUFFFF managed to grid the mapped items

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -14,33 +14,32 @@ const Cards = () => {
     { title: "Tyrasymakhos", img: "plato", id: uniqid() },
     { title: "Socrates", img: "plato", id: uniqid() },
   ]);
-
+  const foo = level1.map((item) => {
+    return <div className="col">{item.title}</div>;
+  });
   return (
     <div>
       <div>{level1[0].id}</div>
-      <ul>
-        {level1.map((item) => {
-          return (
-            <div>
-              <li key={item.id}>
-                {/* {" "}
-                {item.title}, {item.id}, <img src={item.img} /> */}
-                <div className="card" style={{ width: "18rem" }}>
-                  <img src={plato} className="card-img-top" alt="..."></img>
-                  <div className="card-body">
-                    <p className="card-text">
-                      Some quick example text to build on the card title and
-                      make up the bulk of the card's content.
-                    </p>
-                  </div>
-                </div>
-              </li>
-            </div>
-          );
-        })}
-      </ul>
+
+      <div className="row">{foo}</div>
     </div>
   );
 };
 
 export default Cards;
+
+{
+  /* <div classNameName="container">
+              <div classNameName="row">
+                <div classNameName="card col-4" style={{ width: "18rem" }}>
+                  <img src={plato} classNameName="card-img-top" alt="..."></img>
+                  <div classNameName="card-body">
+                    <p classNameName="card-text">
+                      Some quick example text to build on the card title and
+                      make up the bulk of the card's content.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div> */
+}
