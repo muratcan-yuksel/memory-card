df44141b2cf244bf9f461ec5320d576ed7152a07
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-30 21:54:30 +0300

    managed to map an image

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,28 +1,33 @@
 import React, { useState } from "react";
 import uniqid from "uniqid";
+import plato from "../images/plato.jpeg";
 
 const Cards = () => {
   const [level1, setLevel1] = useState([
-    { title: "Plato", src: "", id: uniqid() },
-    { title: "Aristotle", src: "", id: uniqid() },
-    { title: "Tyrasymakhos", src: "", id: uniqid() },
-    { title: "Socrates", src: "", id: uniqid() },
+    {
+      title: "Plato",
+      img: "plato",
+      id: uniqid(),
+    },
+    { title: "Aristotle", img: "plato", id: uniqid() },
+    { title: "Tyrasymakhos", img: "plato", id: uniqid() },
+    { title: "Socrates", img: "plato", id: uniqid() },
   ]);
-  //   const tryLog=()=>{
-  //       setLevel1()
-  //   }
-  const foo = () => {
-    level1.map((item) => {
-      return <li key={item.id}> {item.title}</li>;
-    });
-  };
+
   return (
     <div>
       yoho
       <div>{level1[0].id}</div>
       <ul>
         {level1.map((item) => {
-          return <li key={item.id}> {item.title}</li>;
+          return (
+            <div>
+              <li key={item.id}>
+                {" "}
+                {item.title}, {item.id}, <img src={plato} />
+              </li>
+            </div>
+          );
         })}
       </ul>
     </div>
