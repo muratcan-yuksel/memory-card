134bc12ca59a356995d1b894ecc5c50462c8b0cf
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-05-30 19:14:28 +0300

    added scores and borders UI, also a logo png so that it wouldn't give a stupid error

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -4,6 +4,12 @@ const ScoreBoard = () => {
   return (
     <div>
       <div className="jumbotron">jumbo</div>
+      <div className="container">
+        <div className="row">
+          <div className="col-6 border borderprimary">Currect Score:</div>
+          <div className="col-6 border border-danger">Best Score: </div>
+        </div>
+      </div>
     </div>
   );
 };
